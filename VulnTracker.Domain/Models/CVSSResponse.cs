c9af9ddb8c56a229a2ece349bbab57d281934fc5
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 00:13:55 +0300

    CVSSResponse Created

---
+++ VulnTracker.Domain/Models/CVSSResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VulnTracker.Domain.Models
+{
+    public class CVSSResponse
+    {
+        public double FinalScore { get; set; } // Nihai puan
+        public string Severity { get; set; } // Ciddiyet seviyesi
+    }
+}
