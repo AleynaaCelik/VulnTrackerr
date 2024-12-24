c89fae4a9910a87f4cee5514d13c1b6664c691ed
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 00:16:50 +0300

    CVSSHelper Created

---
+++ VulnTracker.Business/Helpers/CVSSHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VulnTracker.Domain.Models;
+
+namespace VulnTracker.Business.Helpers
+{
+    public static class CVSSHelper
+    {
+        public static CVSSResponse CalculateCVSS(CVSSRequest request)
+        {
+            // Nihai puan hesaplama formülü
+            var finalScore = request.BaseScore * request.TemporalScore * request.EnvironmentalScore;
+
+            // Ciddiyet seviyesini belirleme
+            var severity = finalScore switch
+            {
+                <= 3.9 => "Low",
+                <= 6.9 => "Medium",
+                <= 8.9 => "High",
+                _ => "Critical"
+            };
+
+            return new CVSSResponse
+            {
+                FinalScore = Math.Round(finalScore, 2),
+                Severity = severity
+            };
+        }
+    }
+}
+
+
