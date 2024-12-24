5dcd1dcec7b67d4982123559b70954d7cbdeca99
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 00:03:25 +0300

    Swagger Added

--- VulnTracker.WebApi/Program.cs
+++ VulnTracker.WebApi/Program.cs
@@ -42,26 +42,20 @@ builder.Services.AddSwaggerGen(c =>
     // JWT Bearer Token için Swagger ayarlarý
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
         Name = "Authorization",
-        In = ParameterLocation.Header,
         Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authentication için 'Bearer [token]' formatýný kullanýn."
     });
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
             new OpenApiSecurityScheme
             {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
             },
             new List<string>()
         }
