854405e3e3588a05180f2cb0c29bbca5341fbe8e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 00:22:41 +0300

    CVSSController Created

---
+++ VulnTracker.WebApi/Controllers/CVSSController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VulnTracker.Business.Helpers;
+using VulnTracker.Domain.Models;
+
+namespace VulnTracker.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CVSSController : ControllerBase
+    {
+        // CVSS Hesaplama Endpoint
+        [HttpPost("calculate")]
+        public IActionResult CalculateCVSS([FromBody] CVSSRequest request)
+        {
+            if (request.BaseScore <= 0 || request.TemporalScore <= 0 || request.EnvironmentalScore <= 0)
+            {
+                return BadRequest("All scores must be greater than 0.");
+            }
+
+            var result = CVSSHelper.CalculateCVSS(request);
+            return Ok(result);
+        }
+    }
+}
