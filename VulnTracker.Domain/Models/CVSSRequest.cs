7be5d02846c21788e60605ec4567437a3ae389c7
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 00:13:18 +0300

    CVSSRequest Created

---
+++ VulnTracker.Domain/Models/CVSSRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VulnTracker.Domain.Models
+{
+    public class CVSSRequest
+    {
+        public double BaseScore { get; set; } // Taban puanı
+        public double TemporalScore { get; set; } // Zamana bağlı puan
+        public double EnvironmentalScore { get; set; } // Çevresel puan
+    }
+}
