613d4ddfce5f9a10ff5c801a14eec4f7d97366a6
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 19:33:51 +0300

    GlobalExceptionMiddlewareExtensions Created

---
+++ VulnTracker.WebApi/Middlewares/GlobalExceptionMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace VulnTracker.WebApi.Middlewares
+{
+    public static class GlobalExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<GlobalExceptionMiddleware>();
+        }
+    }
+}
