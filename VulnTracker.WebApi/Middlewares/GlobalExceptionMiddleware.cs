9523e0b0af8f54879c696fb7aed9e0c9ff9ceb43
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-25 19:32:42 +0300

    GlobalExceptionMiddleware Created

---
+++ VulnTracker.WebApi/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace VulnTracker.WebApi.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred. Please try again later.",
+                Detailed = exception.Message // Production ortamında kaldırılabilir.
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(response);
+            return context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
+
